7684f6798f8d28c5d3bb656779a3aecda65a4d49
alex <blackterrapin@gmail.com>
2020-08-04 12:13:46 +0900

    updated authentication to read jwt token

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from './user';
+
+declare function require(name:string);
+var jose = require('jose');
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,12 @@ export class AuthenticationService {
   baseUrl = environment.baseUrl;
   tokens: Tokens;
   currentUser: User = { email: "john@gmail.com", id: "1234", imgUrl: "https://lh3.googleusercontent.com/ogw/ADGmqu-JMRosn04hKyKrbDQBwqHnpZZw9ZBq6tf19tA=s32-c-mo", realName: "Johnnie" };
+
+  userLoginSuccess = new EventEmitter<User>(true);
+  userLoginFail = new EventEmitter<any>(true);
+  
+  
+
 
   constructor(private http: HttpClient) { 
 
@@ -22,12 +31,42 @@ export class AuthenticationService {
         .set('authCode', code)
       }).subscribe(tokens => {
         this.tokens = tokens;
+        this.setUser(this.tokens.idToken);
         console.log(tokens);
       }, error =>{
         console.log(error);
       });
   }
 
+  private setUser(idToken: string) {
+    var decodedJwt = jose.decode(idToken);
+    var userData = decodedJwt.payload as GoogleData;
+    this.currentUser = { email: userData.email, id: userData.sub, imgUrl: userData.picture, realName: userData.name };
+    this.userLoginSuccess.emit(this.currentUser);
+  }
+
+}
+
+
+// "iss": "https://accounts.google.com",
+//   "azp": "407408718192.apps.googleusercontent.com",
+//   "aud": "407408718192.apps.googleusercontent.com",
+//   "sub": "114802719992795296332",
+//   "email": "blackterrapin@gmail.com",
+//   "email_verified": true,
+//   "at_hash": "tYN0svt3cAO3ySC3XxaOKQ",
+//   "name": "Alex Aquino",
+//   "picture": "https://lh3.googleusercontent.com/a-/AOh14Gi36ziLTB7gCY2ejEUQpqN4a5-Jb1Wt0H-KgTE=s96-c",
+//   "given_name": "Alex",
+//   "family_name": "Aquino",
+//   "locale": "en",
+//   "iat": 1596205013,
+//   "exp": 1596208613
+interface GoogleData {
+  email: string;
+  name: string;
+  picture: string;
+  sub: string;
 }
 
 export interface Tokens {
